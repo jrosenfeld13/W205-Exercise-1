24faa245bee2f9db6855404dbc2b5041fdfd5df8
Jason Rosenfeld <j.rosen.1392@gmail.com>
2017-10-19 16:07:25 -0400

    data lake

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -7,24 +7,22 @@ cd stage_data
 wget -O flat_files.zip "https://data.medicare.gov/views/bg9k-emty/files/4a66c672-a92a-4ced-82a2-033c28581a90?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip"
 
 unzip flat_files.zip
-rm flat_files.zip
 
 ##Strip the first line of all of our necessary files, rename them, and move them to the parent directory
-tail -n +2 "Hospital General Information.csv" > /hospitals.csv
-tail -n +2 "Timely and Effective Care - Hospital.csv" > /effective_care.csv
-tail -n +2 "Complications and Deaths - Hospital.csv" > /readmissions.csv
-tail -n +2 "Measure Dates.csv" > /measures.csv
-tail -n +2 "hvbp_hcahps_11_10_2016.csv" > /survey_responses.csv
+tail -n +2 "Hospital General Information.csv" > hospitals.csv
+tail -n +2 "Timely and Effective Care - Hospital.csv" > effective_care.csv
+tail -n +2 "Complications and Deaths - Hospital.csv" > readmissions.csv
+tail -n +2 "Measure Dates.csv" > measures.csv
+tail -n +2 "hvbp_hcahps_11_10_2016.csv" > survey_responses.csv
 
 ##Make hadoop directory and fill it with csv files
 hdfs dfs -mkdir /user/w205/hospital_compare
 
-hdfs dfs -put /hospitals.csv /user/w205/hospital_compare
-hdfs dfs -put /effective_care.csv /user/w205/hospital_compare
-hdfs dfs -put /readmissions.csv /user/w205/hospital_compare
-hdfs dfs -put /measures.csv /user/w205/hospital_compare
-hdfs dfs -put /survey_responses.csv /user/w205/hospital_compare
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare
+hdfs dfs -put measures.csv /user/w205/hospital_compare
+hdfs dfs -put survey_responses.csv /user/w205/hospital_compare
 
+rm flat_files.zip
 cd ..
-
-rm stage_data
